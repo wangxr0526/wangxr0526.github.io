7166e7bde03211124cfb516bb66d0b5e0a3fa4fc
Xiaorui Wang <wangxr2018@lzu.edu.cn>
2026-06-02 11:03:50 +0800

    Fix Google Scholar stats workflow

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -4,7 +4,11 @@ import json
 from datetime import datetime
 import os
 
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+google_scholar_id = os.environ.get('GOOGLE_SCHOLAR_ID', '').strip()
+if not google_scholar_id:
+    raise RuntimeError('GOOGLE_SCHOLAR_ID is required')
+
+author: dict = scholarly.search_author_id(google_scholar_id)
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
